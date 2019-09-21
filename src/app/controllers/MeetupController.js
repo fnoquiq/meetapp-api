1673bf9f14a9783103f62a839a45b35e7af97b7b
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-09-21 14:59:49 -0300

    :rocket: Adiciona verificação de datas no agendamento de meetups
    Não deve ser possível cadastrar meetups com datas que já passaram

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,8 +1,15 @@
+import { parseISO, isBefore } from 'date-fns';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
   async store(req, res) {
     const { title, description, location, date, bannerId } = req.body;
+
+    if (isBefore(parseISO(date), new Date())) {
+      return res.status(401).json({
+        error: 'You can only registrer meetups in a future dates',
+      });
+    }
 
     const meetup = await Meetup.create({
       title,
