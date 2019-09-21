08ac7368a07113cb55f1622b29778b7601f4e2e9
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-09-21 15:02:34 -0300

    :rocket: Adiciona validação de campos em Meetup -> Store

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,8 +1,21 @@
 import { parseISO, isBefore } from 'date-fns';
+import * as Yup from 'yup';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      banner_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { title, description, location, date, bannerId } = req.body;
 
     if (isBefore(parseISO(date), new Date())) {
