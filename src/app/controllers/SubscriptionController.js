1ae818f92db777a46c6f3d032e850b2543952980
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-09-24 00:19:44 -0300

    :rocket: Adiciona inscrição em meetups

---
+++ src/app/controllers/SubscriptionController.js
@@ -0,0 +1,38 @@
+import { isBefore } from 'date-fns';
+
+import Subscription from '../models/Subscription';
+import Meetup from '../models/Meetup';
+
+class SubscriptionController {
+  async store(req, res) {
+    const user_id = req.userId;
+    const meetup_id = req.params.meetupId;
+
+    const meetup = await Meetup.findByPk(meetup_id);
+
+    if (meetup.user_id === user_id) {
+      return res
+        .status(400)
+        .json({ error: "You can't subscribe in your own meetup" });
+    }
+
+    if (isBefore(meetup.date, new Date())) {
+      return res
+        .status(400)
+        .json({ error: "You can't subscribe on past meetups" });
+    }
+
+    const subscription = await Subscription.create({
+      user_id,
+      meetup_id,
+    });
+
+    return res.json(subscription);
+  }
+
+  async index(req, res) {
+    return res.json({ ok: 'true' });
+  }
+}
+
+export default new SubscriptionController();
